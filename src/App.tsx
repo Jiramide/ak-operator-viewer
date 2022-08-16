ac6d16cfed0b71b27c8cf788cf29ee832f87f7f8
Jade Piramide <jadepiramidepogi@gmail.com>
2022-08-16 00:59:24 -0700

    Put nested routing for pages in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,17 @@ import Talent from './pages/Talent';
 import Skill from './pages/Skill';
 import Module from './pages/Module';
 
+
 const App = () => (
   <Router>
     <Routes>
       <Route path='/' element={ <Home /> }>
-          <Route path='operator/:operator/' element={ <Operator /> }>
-            <Route path='talent/' element={ <Talent /> } />
-            <Route path='skill/' element={ <Skill /> } />
-            <Route path='module/' element={ <Module /> } />
-          </Route>
+        <Route path='operator/:operator/' element={ <Operator /> }>
+          <Route path='talent/' element={ <Talent /> } />
+          <Route path='skill/' element={ <Skill /> } />
+          <Route path='module/' element={ <Module /> } />
         </Route>
+      </Route>
     </Routes>
   </Router>
 )
